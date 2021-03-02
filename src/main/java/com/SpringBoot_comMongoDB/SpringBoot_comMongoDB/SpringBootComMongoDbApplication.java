26104d07bbdef8572f33c7d66373eeed969b69a8
Eduardo <eduardo00valiati@gmail.com>
2021-03-02 18:22:01 -0300

    projeto criado

---
+++ src/main/java/com/SpringBoot_comMongoDB/SpringBoot_comMongoDB/SpringBootComMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_comMongoDB.SpringBoot_comMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot_comMongoDB/SpringBoot_comMongoDB/SpringBootComMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_comMongoDB.SpringBoot_comMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
